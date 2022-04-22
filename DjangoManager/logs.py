ee46294323a3de5e50752c13b62a98c351bcd572
Corban-Lee <843224@student.derby-college.ac.uk>
2022-04-22 16:24:42 +0100

    completed logs

--- DjangoManager/logs.py
+++ DjangoManager/logs.py
@@ -1,3 +1,46 @@
 import logging
+from appdirs import AppDirs
+from itertools import count
+from pathlib import Path
+from typing import TextIO
+from datetime import datetime, timedelta
 
-log = logging.GetLogger()
+
+FILENAME_FORMAT_PREFIX = '%Y-%m-%d %H-%M-%S'
+MAX_LOGFILE_AGE_DAYS = 7
+
+log = logging.getLogger(__name__)
+
+
+def _open_file(dirs:AppDirs) -> TextIO:
+    timestamp = datetime.now().strftime(FILENAME_FORMAT_PREFIX)
+    filenames = (f'{timestamp}.txt' if i == 0 else f'{timestamp}_{i}.txt' for i in count())
+    for filename in filenames:
+        try:
+            return (Path(f'{dirs.user_log_dir}/{filename}').open('x', encoding='utf-8'))
+        except FileExistsError:
+            continue
+        
+def _destroy_old_logs(dirs:AppDirs):
+    for path in Path(dirs.user_log_dir).glob('*.txt'):
+        prefix = path.stem.split('_')[0]
+        try:
+            log_date = datetime.strptime(prefix, FILENAME_FORMAT_PREFIX)
+        except ValueError:
+            log.warning(f'{path.parent} contains a problematic filename: {path.name}')
+            continue
+        
+        age = datetime.now() - log_date
+        if age >= timedelta(days=MAX_LOGFILE_AGE_DAYS):
+            log.info(f'removing {path.name} from logs at {path.parent} because the file is older than {MAX_LOGFILE_AGE_DAYS} days')
+            path.unlink()
+
+def setup(appname:str, version:str, dirs:AppDirs) -> None:
+    
+    file = _open_file(dirs)
+    handler = logging.StreamHandler(file)
+    handler.setLevel(logging.DEBUG)
+    handler.setFormatter(logging.Formatter('[%(asctime)s] %(name)s %(levelname)s: %(message)s'))
+    logging.basicConfig(level=logging.DEBUG, handlers=[handler])
+    log.debug(f'Starting {appname} {version}')
+    _destroy_old_logs(dirs)
