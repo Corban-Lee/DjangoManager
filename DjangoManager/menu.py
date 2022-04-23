ee62c535c6d0eb8e2e66c569547a562065a44879
Corban-Lee <843224@student.derby-college.ac.uk>
2022-04-23 22:43:56 +0100

    added debug menu

--- DjangoManager/menu.py
+++ DjangoManager/menu.py
@@ -5,22 +5,23 @@ import logging
 log = logging.getLogger(__name__)
 
 
-class MenuManager:
+class MenuManager():
     
     def __init__(self, root):
-        log.info('creating menu bar for app window')
+        log.info('initialising menubar manager')
         
         self.menu = tkinter.Menu(root.window)
         
         # filemenu
         filemenu = tkinter.Menu(self.menu, tearoff=False)
+        self.menu.add_cascade(menu=filemenu, label='File')
+        
         filemenu.add_command(label='New Project', command=None)
         filemenu.add_command(label='Open Project', command=None)
         filemenu.add_separator()
         filemenu.add_command(label='Save Project', command=None)
         filemenu.add_separator()
         filemenu.add_command(label='Close Project', command=None)
-        self.menu.add_cascade(menu=filemenu, label='File')
         
         # editmenu
         editmenu = tkinter.Menu(self.menu, tearoff=False)
@@ -29,5 +30,11 @@ class MenuManager:
         # viewmenu
         viewmenu = tkinter.Menu(self.menu, tearoff=False)
         self.menu.add_cascade(menu=viewmenu, label='View')
+        
+        # debugmenu
+        debugmenu = tkinter.Menu(self.menu, tearoff=False)
+        self.menu.add_cascade(menu=debugmenu, label='Debug')
+        
+        debugmenu.add_command(label='Add Empty Tab', command=lambda:root.tabs.add_tab(f'Tab Number {len(root.tabs.tabs)}'))
         
         root.window.config(menu=self.menu)
