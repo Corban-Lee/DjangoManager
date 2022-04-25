cb6f3fbdd8c5332582e8bca36582aee5a2e3853a
Corban-Lee <77944149+XordK@users.noreply.github.com>
2022-04-25 14:50:58 +0100

    Create controls.py

---
+++ DjangoManager/controls.py
@@ -0,0 +1,34 @@
+import logging
+import tkinter
+from tkinter import ttk
+
+
+log = logging.getLogger(__name__)
+
+
+class ControlFrame(ttk.Frame):
+    def __init__(self, root):
+        super().__init__(root.paned_window, style='Control.TFrame')
+        
+        paned_window = tkinter.PanedWindow(self, orient='horizontal', background='#D9D9D9', sashwidth=5)
+        paned_window.pack(fill='both', expand=True)
+        
+        console_pane = ttk.Frame(paned_window)
+        console_pane.pack(side='left', fill='both')
+        paned_window.add(console_pane, minsize=100, stretch='always')
+ 
+        
+        manage_pane = ttk.Frame(paned_window)
+        manage_pane.pack(side='left', fill='both', expand=True)
+        paned_window.add(manage_pane, minsize=100, stretch='always')
+        
+        left_sep = ttk.Frame(manage_pane, style='TabSeparator.TFrame', width=1)
+        left_sep.pack(side='left', fill='y')
+        
+        right_sep = ttk.Frame(manage_pane, style='TabSeparator.TFrame', width=1)
+        right_sep.pack(side='right', fill='y')
+        
+        admin_pane = ttk.Frame(paned_window)
+        admin_pane.pack(side='left', fill='both', expand=True)
+        paned_window.add(admin_pane, minsize=100, stretch='always')
+
