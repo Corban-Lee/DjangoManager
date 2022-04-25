d39c0dd84810c7206dfc8e19d6fc8b7c14e660a1
Corban-Lee <843224@student.derby-college.ac.uk>
2022-04-25 04:15:27 +0100

    changed the manager to inherit from tkinter.Menu

--- DjangoManager/menu.py
+++ DjangoManager/menu.py
@@ -5,16 +5,15 @@ import logging
 log = logging.getLogger(__name__)
 
 
-class MenuManager():
+class MenuManager(tkinter.Menu):
     
     def __init__(self, root):
+        super().__init__(root.window)
         log.info('initialising menubar manager')
-        
-        self.menu = tkinter.Menu(root.window)
-        
+
         # filemenu
-        filemenu = tkinter.Menu(self.menu, tearoff=False)
-        self.menu.add_cascade(menu=filemenu, label='File')
+        filemenu = tkinter.Menu(self, tearoff=False)
+        self.add_cascade(menu=filemenu, label='File')
         
         filemenu.add_command(label='New Project', command=None)
         filemenu.add_command(label='Open Project', command=None)
@@ -24,17 +23,17 @@ class MenuManager():
         filemenu.add_command(label='Close Project', command=None)
         
         # editmenu
-        editmenu = tkinter.Menu(self.menu, tearoff=False)
-        self.menu.add_cascade(menu=editmenu, label='Edit')
+        editmenu = tkinter.Menu(self, tearoff=False)
+        self.add_cascade(menu=editmenu, label='Edit')
         
         # viewmenu
-        viewmenu = tkinter.Menu(self.menu, tearoff=False)
-        self.menu.add_cascade(menu=viewmenu, label='View')
+        viewmenu = tkinter.Menu(self, tearoff=False)
+        self.add_cascade(menu=viewmenu, label='View')
         
         # debugmenu
-        debugmenu = tkinter.Menu(self.menu, tearoff=False)
-        self.menu.add_cascade(menu=debugmenu, label='Debug')
+        debugmenu = tkinter.Menu(self, tearoff=False)
+        self.add_cascade(menu=debugmenu, label='Debug')
         
         debugmenu.add_command(label='Add Empty Tab', command=lambda:root.tabs.add_tab(f'Tab Number {len(root.tabs.tabs)}'))
         
-        root.window.config(menu=self.menu)
+        root.window.config(menu=self)
