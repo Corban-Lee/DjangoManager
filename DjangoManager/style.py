116d48ce03225ed21e6060b367b8757e907f17be
Corban-Lee <843224@student.derby-college.ac.uk>
2022-04-25 03:58:01 +0100

    Made tabs work + more (see desc)
    Added 'x' images for tab close button.
    Added style options for tabs.
    Made tabs actually work.
    Added loads of comments.

--- DjangoManager/style.py
+++ DjangoManager/style.py
@@ -19,29 +19,79 @@ class StyleManager(ttk.Style):
                         ('Button.label', {'sticky': 'nswe'})
             ]})]})])
         
-        # style for tabs
+        ###########
+        
+        # Styles for tabs
+        
+        # the trough contains tabs
         self.configure(
             'TabTrough.TFrame',
             background='#E9E9E9'
         )
+        
+        # the tabs themselves
         self.configure(
             'Tab.TFrame',
             border=1,
+            relief='flat',
             background='#F1F1F1'
         )
+        
+        # the text on the tabs
         self.configure(
             'TabText.TLabel',
             background='#F1F1F1'
         )
+        
+        # the close button for the tabs
         self.configure(
-            'TabClose.TButton',
-            borderwidth=0,
-            highlightthickness=0,
-            focuscolor='#F1F1F1',
+            'TabBtn.TLabel',
             background='#F1F1F1',
-            foreground='black'
+            padding=(5, 0, 5, 0)
         )
+        self.map(
+            'TabBtn.TLabel',
+            background=(
+                ('pressed', '#F1F1F1'), 
+                ('active', '#F1F1F1')
+            )
+        )
+        
+        # reserved space is a sunken looking color that occupies the emptied space when
+        # a tab is actively being moved.
         self.configure(
             'TabReserved.TFrame',
-            background='#E9E9E9'
+            background='#DFDFDF'
         )
+        
+        ########
+        
+        # Alternative tab styles (for when tabs are selected/active/whatever)
+        
+        self.configure(
+            'SelectedTab.TFrame',
+            background='gray',
+            relief='flat'
+        )
+        
+        self.configure(
+            'SelectedTabText.TLabel',
+            background='gray',
+            foreground='white'
+        )
+        
+        self.configure(
+            'SelectedTabBtn.TLabel',
+            background='gray',
+            padding=(5, 0, 5, 0)
+        )
+        self.map(
+            'SelectedTabBtn.TLabel',
+            background=(
+                ('pressed', 'red'), 
+                ('active', 'orange')
+            )
+        )
+        
+        
+        ##########
--- DjangoManager/tabs.py
+++ DjangoManager/tabs.py
@@ -1,30 +1,57 @@
+from fileinput import close
 import logging
 import tkinter
 from tkinter import ttk
+from PIL import Image, ImageTk
 
 
 log = logging.getLogger(__name__)
 
 
+IMAGES_DIR = 'DjangoManager/images'
+
+
 class Tab(ttk.Frame):
+    
+    selected: bool = False
 
     def __init__(self, master, text:str):
-        super().__init__(master, style='Tab.TFrame')
-        self.pack(side='left', fill='y')
+        super().__init__(master, style=f'Tab.TFrame')
+        self.bind('<Button-1>', self.drag)
         
-        # tab label
-        self.text = ttk.Label(self, text=text, style='TabText.TLabel')
-        self.text.pack(side='left', fill='both', expand=True)
+        self.text = ttk.Label(self, text=text, style=f'TabText.TLabel')
+        self.text.pack(side='left', fill='both', expand=True, padx=15, pady=1)
         self.text.bind('<Button-1>', self.drag)
         
-        # close button
-        self.btn = ttk.Button(self, text='X', style='TabClose.TButton', width=5, command=self._destroy)
-        self.btn.pack(side='right', fill='y')
+        # get 'x' image for close button
+        img = Image.open(f'{IMAGES_DIR}/tab_close_dark.png')
+        img = img.resize((15,15), Image.ANTIALIAS)
+        close_btn_img = ImageTk.PhotoImage(img)
         
-    def _calculate_start_pos(self):
-        length = 0
-        for tab in self.master.tabs:
-            length += tab.winfo_width()
+        self.close_btn = ttk.Button(self, image=close_btn_img, style='TabBtn.TLabel', command=self._destroy)
+        self.close_btn.image = close_btn_img  # tk is dumb and requires an attribute of the Image obj. Attr name doesnt matter.
+        self.close_btn.pack(side='right', fill='y', padx=(0, 1), pady=1)
+
+        # reserves a space when moving the tab
+        self.reserved_space = ttk.Frame(self.master, style='TabReserved.TFrame')
+        
+        self.pack(side='left', fill='y')
+        
+    def select(self):
+        assert not self.selected
+        self.selected = True
+        
+        self.config(style='SelectedTab.TFrame')
+        self.text.config(style='SelectedTabText.TLabel')
+        self.close_btn.config(style='SelectedTabBtn.TLabel')
+    
+    def deselect(self):
+        assert self.selected
+        self.selected = False
+        
+        self.config(style='Tab.TFrame')
+        self.text.config(style='TabText.TLabel')
+        self.close_btn.config(style='TabBtn.TLabel')
             
     def _destroy(self):
         self.destroy()
@@ -32,43 +59,81 @@ class Tab(ttk.Frame):
      
     def _snap_to_pos(self, event:tkinter.Event):
         self.pack(side='left', fill='y')
-        
-    def _reserve_space(self) -> ttk.Frame:
-        
-        try:
-            before = self.master.tabs[self.master.tabs.index(self) ]
-        except IndexError:
-            before = None
-        
-        reserve_block = ttk.Frame(
-            self.master,
-            width=self.winfo_width(),
-            style='TabReserved.TFrame'
-        )
-        reserve_block.pack(side='left', fill='y', before=before)
-        return reserve_block
-        
 
     def drag(self, event:tkinter.Event) -> None:
-        reserved = self._reserve_space()
+        """Allows the user to drag the tab"""
+        
+        self.master.select_tab(self)
+        
+        # create reserved space for the widget
+        self.reserved_space.config(width=self.winfo_width())
+        self.reserved_space.pack(side='left', fill='y', before=self)
+        
+        # if the tab isn't raised it can appear below other tabs and reserved space
         self.tkraise()
-        selfx = self.winfo_x() - event.x_root
-        trough_height = self.master.winfo_height() - 3
         
-        def finish(event:tkinter.Event) -> None:
-            reserved.destroy()
-            self._snap_to_pos(event)
+        original_x = self.winfo_x()
+        current_x = original_x - event.x_root
+        trough_height = self.master.winfo_height() - 2  # accounts for margin
         
-        def move(event:tkinter.Event) -> None:
-            newx = event.x_root + selfx
-            if not (newx >= self.master.winfo_width() - self.winfo_width() or newx < 0):
-                self.place(x=newx, h=trough_height)
-    
-            # calculate position to snap to
-            self.master.update()
+        def release(event:tkinter.Event) -> None:
+            """Left mouse button is released"""
+            
+            # 'x' coordinate, used to determine where to put the widget when dropped
+            x: int = int(self.winfo_x() + (self.winfo_width() / 3))
 
-        self.text.bind('<B1-Motion>', move)
-        self.text.bind('<ButtonRelease-1>', finish)
+            # create a list of everything on the tab trough excluding self
+            items = self.master.tabs.copy()
+            items.append(self.reserved_space)
+            items.remove(self)
+
+            
+            for item in items:
+                
+                # is self overlapping item?
+                tab_x = item.winfo_x()
+                if x in range(tab_x, tab_x + item.winfo_width()):
+                    break
+                
+            # self is not overlapping any items: move it to the end of
+            # the trough.
+            else:
+                self.pack(side='left', fill='y')
+                self.reserved_space.pack_forget()
+                return
+                
+            # now that we have confirmed self is hovering an item we need
+            # to determine if self has moved from left to right or vise versa.
+            #
+            # 'original_x' is the pos of self before the user clicked & dragged
+            # on it.
+            #
+            # 'x' is the current x coordinate of self.
+            
+            # self moved left to right
+            if x > original_x:
+                self.pack(side='left', fill='y', after=item)
+                
+            # self moved right to left
+            else:
+                self.pack(side='left', fill='y', before=item)
+
+            self.reserved_space.pack_forget()
+
+        def move(event:tkinter.Event) -> None:
+            """Cursor is moved while left mouse button is active"""
+            
+            new_x = event.x_root + current_x
+            
+            # prevent tab going past the trough border
+            if not (new_x >= self.master.winfo_width() - self.winfo_width() or new_x < 0):
+                self.place(x=new_x, h=trough_height)
+    
+            # TODO: did this to reduce fps lag. Don't know if it actually works. Test it?
+            self.update()
+
+        event.widget.bind('<B1-Motion>', move)
+        event.widget.bind('<ButtonRelease-1>', release)
         move(event)
 
         
@@ -83,16 +148,19 @@ class TabManager(ttk.Frame):
         self.pack_propagate(False)
         self.pack(side='top', fill='x')
         
-        ttk.Separator(self, orient='horizontal').pack(side='bottom', fill='x', pady=(1,0))
-        
-        self.add_tab(text='This is a tab')
-        self.add_tab(text='this is another tab')
-        self.add_tab(text='third tab here')
-        
+        ttk.Separator(self, orient='horizontal').pack(side='bottom', fill='x')
         
     def add_tab(self, text:str):
         self.tabs.append(Tab(self, text))
         
+    def select_tab(self, tab_to_select:Tab):
+        for tab in self.tabs:
+            if tab.selected:
+                tab.deselect()
+                break
+            
+        tab_to_select.select()
+        print('new tab selected')
     
     def remove_tab(self):
         pass
