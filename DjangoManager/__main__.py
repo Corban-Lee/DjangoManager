d1c5e15d0cb2ba88c96a00d052531c662efa7d88
Corban-Lee <77944149+XordK@users.noreply.github.com>
2022-04-25 14:51:20 +0100

    updated variable names

--- DjangoManager/__main__.py
+++ DjangoManager/__main__.py
@@ -3,11 +3,12 @@ import logging
 from pathlib import Path
 from appdirs import AppDirs
 
-import logs
+import _logs
 from tabs import TabManager
 from cfg import ConfigManager
 from menu import MenuManager
 from style import StyleManager
+from controls import ControlFrame
 
 
 log = logging.getLogger(__name__)
@@ -21,7 +22,7 @@ class Root:
     name = 'DjangoManager'  # name shouldn't contain spaces or special characters
     window = tkinter.Tk()
     dirs = AppDirs(name, __author__, __version__)
-    logs.setup(name, __version__, dirs)
+    _logs.setup(name, __version__, dirs)
 
     def __init__(self):
         
@@ -32,12 +33,24 @@ class Root:
         self.window.minsize(700, 400)
         self.window.title(self.name)
         
+        self.paned_window = tkinter.PanedWindow(self.window, orient='vertical', sashwidth=3, background='#D9D9D9')
+        self.paned_window.pack(fill='both', expand=True)
+        
+        self.control_frame = ControlFrame(self)
+        self.control_frame.pack(side='bottom', fill='both', expand=True)
+        
         # Get & setup managers
-        self.style = StyleManager(self)  # must be called first
         self.cfg = ConfigManager(self)
+        self.style = StyleManager(self)  # must be called first
         self.tabs = TabManager(self)
         self.menu = MenuManager(self)
-    
+        
+        self.cfg.restore_defaults()
+        
+        self.paned_window.add(self.tabs, minsize=25)
+        self.paned_window.add(self.control_frame, minsize=200)
+
+
     def run(self) -> None:
         log.info('starting window main loop')
         self.window.mainloop()
@@ -53,8 +66,8 @@ class Root:
         Path(self.dirs.user_cache_dir).mkdir(parents=True, exist_ok=True)
         Path(self.dirs.user_config_dir).mkdir(parents=True, exist_ok=True)
         Path(self.dirs.user_log_dir).mkdir(parents=True, exist_ok=True)
-        
-        
+
+
 if __name__ == '__main__':
     Root().run()
     
