d51a00433feea090eb056915e1f06acb2b2da8de
Corban-Lee <77944149+XordK@users.noreply.github.com>
2022-04-26 16:04:16 +0100

    created text length check utility

--- DjangoManager/utils.py
+++ DjangoManager/utils.py
@@ -8,3 +8,10 @@ def get_all_children(widget:tkinter.Widget|tkinter.Tk) -> list[tkinter.Widget]:
     for child in children:
         children.extend(child.winfo_children())
     return children
+
+
+def text_length_check(text:str, length:int):
+    """Replaces long strings with a shortened copy ending in '...'"""
+    if len(text) > length:
+        return text[:length - 3] + '...'
+    return text
