49a902e7d2e289deb529f7a09a044e6a50abfe9b
Corban-Lee <77944149+XordK@users.noreply.github.com>
2022-04-27 12:13:25 +0100

    changed panel_window bg

--- DjangoManager/controls.py
+++ DjangoManager/controls.py
@@ -13,18 +13,20 @@ class ControlFrame(ttk.Frame):
         wrapper = ttk.Frame(self)
         wrapper.place(x=0, rely=.5, relw=1, relh=.75, anchor='w')
         
-        paned_window = tkinter.PanedWindow(wrapper, orient='horizontal', background='#D9D9D9', sashwidth=5)
+        paned_window = tkinter.PanedWindow(wrapper, orient='horizontal', background='#E4E4E4', sashwidth=5)
         paned_window.pack(fill='both', expand=True)
         
-        
+        # Project Details Pane
         details_pane = ttk.Frame(paned_window)
         details_pane.pack(side='left', fill='both')
         paned_window.add(details_pane, minsize=100, stretch='always')
 
         details_header = ttk.Label(details_pane, text='Project Details', style='Header.TLabel')
         details_header.pack(side='top', anchor='w')
- 
+
         
+ 
+        # Management Pane       
         manage_pane = ttk.Frame(paned_window)
         manage_pane.pack(side='left', fill='both', expand=True)
         paned_window.add(manage_pane, minsize=100, stretch='always')
@@ -38,7 +40,7 @@ class ControlFrame(ttk.Frame):
         right_sep = ttk.Frame(manage_pane, style='TabSeparator.TFrame', width=1)
         right_sep.place(relx=1, rely=.5, relh=1, anchor='e')
         
-
+        # Administration Pane
         admin_pane = ttk.Frame(paned_window)
         admin_pane.pack(side='left', fill='both', expand=True)
         paned_window.add(admin_pane, minsize=100, stretch='always')
