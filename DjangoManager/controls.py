b46e293c3382c04c63a5fbfc00d55fdfb06fd961
Corban-Lee <843224@student.derby-college.ac.uk>
2022-05-02 01:40:24 +0100

    remade the entire control area

--- DjangoManager/controls.py
+++ DjangoManager/controls.py
@@ -9,42 +9,53 @@ log = logging.getLogger(__name__)
 class ControlFrame(ttk.Frame):
     def __init__(self, root):
         super().__init__(root.paned_window, style='Control.TFrame')
-
-        wrapper = ttk.Frame(self)
-        wrapper.place(x=0, rely=.5, relw=1, relh=.75, anchor='w')
         
-        paned_window = tkinter.PanedWindow(wrapper, orient='horizontal', background='#E4E4E4', sashwidth=5)
+        paned_window = tkinter.PanedWindow(self, orient='horizontal', background='#a3a2a2', sashwidth=1, bd=0)
         paned_window.pack(fill='both', expand=True)
         
-        # Project Details Pane
-        details_pane = ttk.Frame(paned_window)
-        details_pane.pack(side='left', fill='both')
-        paned_window.add(details_pane, minsize=100, stretch='always')
-
-        details_header = ttk.Label(details_pane, text='Project Details', style='Header.TLabel')
-        details_header.pack(side='top', anchor='w')
-
+        project_pane = ttk.Frame(paned_window)
+        project_pane.columnconfigure(index=0, weight=1)
+        paned_window.add(project_pane, minsize=200)
+        paned_window.add(ttk.Frame(paned_window), minsize=300)
         
- 
-        # Management Pane       
-        manage_pane = ttk.Frame(paned_window)
-        manage_pane.pack(side='left', fill='both', expand=True)
-        paned_window.add(manage_pane, minsize=100, stretch='always')
-
-        manage_header = ttk.Label(manage_pane, text='Management', style='Header.TLabel')
-        manage_header.pack(side='top', anchor='w')
+        self.project_header = tkinter.StringVar(value='Project Title Here')
+        self.project_path = tkinter.StringVar(value='C:/path/to/project')
+        self.environment_path = tkinter.StringVar(value='C:/path/to/environment')
         
-        left_sep = ttk.Frame(manage_pane, style='TabSeparator.TFrame', width=1)
-        left_sep.place(x=0, rely=.5, relh=1, anchor='w')
+        # project header
+        ttk.Label(project_pane, textvariable=self.project_header, style='Header.TLabel'
+        ).grid(column=0, row=0, sticky='we', padx=15, pady=15)
         
-        right_sep = ttk.Frame(manage_pane, style='TabSeparator.TFrame', width=1)
-        right_sep.place(relx=1, rely=.5, relh=1, anchor='e')
+        # project header (control)
+        ttk.Label(project_pane, text='Project Title'
+        ).grid(column=0, row=1, sticky='w', padx=15, pady=(0, 3))
+        ttk.Entry(project_pane, textvariable=self.project_header
+        ).grid(column=0, row=2, sticky='we', padx=15, pady=(0, 15))
+       
+        # project path
+        ttk.Label(project_pane, text='Project Location'
+        ).grid(column=0, row=3, sticky='w', padx=15, pady=(0, 3))
+        ttk.Entry(project_pane, textvariable=self.project_path,
+        ).grid(column=0, row=4, sticky='we', padx=15, pady=(0, 15))
         
-        # Administration Pane
-        admin_pane = ttk.Frame(paned_window)
-        admin_pane.pack(side='left', fill='both', expand=True)
-        paned_window.add(admin_pane, minsize=100, stretch='always')
-
-        admin_header = ttk.Label(admin_pane, text='Administration', style='Header.TLabel')
-        admin_header.pack(side='top', anchor='w')
-
+        # environment path
+        ttk.Label(project_pane, text='Environment Location',
+        ).grid(column=0, row=5, sticky='w', padx=15, pady=(0, 3))
+        ttk.Entry(project_pane, textvariable=self.environment_path,
+        ).grid(column=0, row=6, sticky='we', padx=15, pady=(0, 15))
+        
+        # update attributes
+        btn_border = ttk.Frame(project_pane, style='BtnBorder.TFrame')
+        btn_border.grid(column=0, row=7, sticky='we', padx=15, pady=(5, 15))
+        ttk.Button(btn_border, text='Save Changes'
+        ).pack(fill='both', expand=True, padx=1, pady=1)
+        
+        
+        ###########################################
+        # console pane
+        
+        console_pane = ttk.Frame(paned_window)
+        paned_window.add(console_pane, minsize=200)
+        
+        console = tkinter.Text(console_pane, width=1, height=1, bd=0)
+        console.pack(fill='both', expand=True)
