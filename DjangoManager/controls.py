19d66f05e230c6fc7a4d1be21a369bc72c50f1c9
Corban-Lee <77944149+XordK@users.noreply.github.com>
2022-05-03 16:31:05 +0100

    added auto border adder

--- DjangoManager/controls.py
+++ DjangoManager/controls.py
@@ -2,6 +2,8 @@ import logging
 import tkinter
 from tkinter import ttk
 
+from utils import clone_widget
+
 
 log = logging.getLogger(__name__)
 
@@ -45,11 +47,27 @@ class ControlFrame(ttk.Frame):
         ).grid(column=0, row=6, sticky='we', padx=15, pady=(0, 15))
         
         # update attributes
-        btn_border = ttk.Frame(project_pane, style='BtnBorder.TFrame')
-        btn_border.grid(column=0, row=7, sticky='we', padx=15, pady=(5, 15))
-        ttk.Button(btn_border, text='Save Changes'
-        ).pack(fill='both', expand=True, padx=1, pady=1)
-        
+        # btn_border = ttk.Frame(project_pane, style='Border.TFrame')
+        # btn_border.grid(column=0, row=7, sticky='we', padx=15, pady=(5, 15))
+        ttk.Button(project_pane, text='Save Changes'
+        ).grid(column=0, row=7, sticky='we', padx=15, pady=(5, 15))
+        #).pack(fill='both', expand=True, padx=1, pady=1)
+
+        # give all button and entry widgets borders
+        # this is really bad: deletes and reconstructs widgets to do this.
+        for widget in project_pane.winfo_children():
+            if not isinstance(widget, (ttk.Button, ttk.Entry)):
+                continue
+
+            grid_details = widget.grid_info()
+            widget.grid_forget()
+
+            border = ttk.Frame(project_pane, style='Border.TFrame')
+            border.grid(grid_details)
+            
+            widget_clone = clone_widget(widget, parent=border)
+            widget_clone.pack(fill='both', expand=True, padx=1, pady=1)
+            widget.destroy()
         
         ###########################################
         # console pane
