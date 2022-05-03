295cd4a78a9992f9d44ea240e26ca3fdf5bdda8e
Corban-Lee <843224@student.derby-college.ac.uk>
2022-05-03 08:08:36 +0100

    moved to reading themes from json files

--- DjangoManager/constants.py
+++ DjangoManager/constants.py
@@ -1,4 +1,4 @@
-
+THEME_DIR = 'DjangoManager/themes'
 IMAGES_DIR = 'DjangoManager/images'
 CONFIG_FILENAME = 'config.json'
 FILENAME_FORMAT_PREFIX = '%Y-%m-%d %H-%M-%S'
--- DjangoManager/style.py
+++ DjangoManager/style.py
@@ -1,6 +1,8 @@
 import logging
-import tkinter
 from tkinter import ttk
+
+from utils import get_json
+from constants import THEME_DIR
 
 
 log = logging.getLogger(__name__)
@@ -9,7 +11,9 @@ log = logging.getLogger(__name__)
 class StyleManager(ttk.Style):
     def __init__(self, root):
         super().__init__(root.window)
-        self.theme_use('default')
+        
+        self._create_theme('light.json')
+        self.theme_use('light')
         
         # redefined tbutton layout without dotted focus outline
         self.layout(
@@ -19,108 +23,121 @@ class StyleManager(ttk.Style):
                     {'sticky': 'nswe', 'border': '1', 'children': [
                         ('Button.padding', {'sticky': 'nswe', 'children': [
                             ('Button.label', {'sticky': 'nswe'})
-            ]})]})]})])
-        
-        self.configure(
-            'Header.TLabel',
-            font=('TkDefaultFont', 13),
-            padding=(0, 0, 0, 15),
-        )
-        
-        self.configure('TLabel', background='#E4E4E4')
-
-        self.configure('TabSeparator.TFrame', background='#a5a4a5')
-        
-        self.configure('TFrame', background='#E4E4E4')
-        self.paned_window = {
-            'background': '#E4E4E4',
-            'sashwidth': 3
-        }
-        
-        self.configure('TButton', borderwidth=0)
-        self.configure('BtnBorder.TFrame', background='#a5a4a5')
-        
-        self.configure(
-            'TEntry',
-            background='gray90',
-            borderwidth=0,
-            padding=(5,2,5,2)
-        )
-        
-        ###########
-        
-        # Styles for tabs
-        
-        # the trough contains tabs
-        self.configure(
-            'TabTrough.TFrame',
-            background='gray95'
-        )
-        
-        # the tabs themselves
-        self.configure(
-            'Tab.TFrame',
-            border=1,
-            relief='flat',
-            background='#D9D9D9'
-        )
-        
-        # the text on the tabs
-        self.configure(
-            'TabText.TLabel',
-            background='#D9D9D9',
-            padding=(5, 1, 25, 0)
-        )
-
-        # the close button for the tabs
-        self.configure(
-            'TabBtn.TLabel',
-            background='#D9D9D9',
-            padding=(5, 0, 5, 0)
-        )
-        self.map(
-            'TabBtn.TLabel',
-            background=(
-                ('pressed', 'gray75'), 
-                ('active', 'gray80')
+            ]})]})]})]
             )
+        
+    def _create_theme(self, filename:str):
+        """
+            Looks for argument filename in themes directory and 
+            attempts to load it.
+        """
+        data = get_json(THEME_DIR + '/' + filename)
+        self.theme_create(
+            themename=filename.removesuffix('.json'),
+            parent=data['parent'],
+            settings=data['ttk']
         )
         
-        # reserved space is a sunken looking color that occupies the emptied space when
-        # a tab is actively being moved.
-        self.configure(
-            'TabReserved.TFrame',
-            background='gray45'
-        )
+        # self.configure(
+        #     'Header.TLabel',
+        #     font=('TkDefaultFont', 13),
+        #     padding=(0, 0, 0, 15),
+        # )
         
-        ########
+        # self.configure('TLabel', background='#E4E4E4')
+
+        # self.configure('TabSeparator.TFrame', background='#a5a4a5')
         
-        # Alternative tab styles (for when tabs are selected/active/whatever)
+        # self.configure('TFrame', background='#E4E4E4')
+        # self.paned_window = {
+        #     'background': '#E4E4E4',
+        #     'sashwidth': 3
+        # }
         
-        self.configure(
-            'SelectedTab.TFrame',
-            background='gray70',
-            relief='flat'
-        )
+        # self.configure('TButton', borderwidth=0)
+        # self.configure('BtnBorder.TFrame', background='#a5a4a5')
         
-        self.configure(
-            'SelectedTabText.TLabel',
-            background='gray70',
-            padding=(5, 1, 25, 0)
-        )
+        # self.configure(
+        #     'TEntry',
+        #     background='gray90',
+        #     borderwidth=0,
+        #     padding=(5,2,5,2)
+        # )
         
-        self.configure(
-            'SelectedTabBtn.TLabel',
-            background='gray70',
-            padding=(5, 0, 5, 0)
-        )
-        self.map(
-            'SelectedTabBtn.TLabel',
-            background=(
-                ('pressed', 'gray60'), 
-                ('active', 'gray65')
-            )
-        )
+        # ###########
+        
+        # # Styles for tabs
+        
+        # # the trough contains tabs
+        # self.configure(
+        #     'TabTrough.TFrame',
+        #     background='gray95'
+        # )
+        
+        # # the tabs themselves
+        # self.configure(
+        #     'Tab.TFrame',
+        #     border=1,
+        #     relief='flat',
+        #     background='#D9D9D9'
+        # )
+        
+        # # the text on the tabs
+        # self.configure(
+        #     'TabText.TLabel',
+        #     background='#D9D9D9',
+        #     padding=(5, 1, 25, 0)
+        # )
+
+        # # the close button for the tabs
+        # self.configure(
+        #     'TabBtn.TLabel',
+        #     background='#D9D9D9',
+        #     padding=(5, 0, 5, 0)
+        # )
+        # self.map(
+        #     'TabBtn.TLabel',
+        #     background=(
+        #         ('pressed', 'gray75'), 
+        #         ('active', 'gray80')
+        #     )
+        # )
+        
+        # # reserved space is a sunken looking color that occupies the emptied space when
+        # # a tab is actively being moved.
+        # self.configure(
+        #     'TabReserved.TFrame',
+        #     background='gray45'
+        # )
+        
+        # ########
+        
+        # # Alternative tab styles (for when tabs are selected/active/whatever)
+        
+        # self.configure(
+        #     'SelectedTab.TFrame',
+        #     background='gray70',
+        #     relief='flat'
+        # )
+        
+        # self.configure(
+        #     'SelectedTabText.TLabel',
+        #     background='gray70',
+        #     padding=(5, 1, 25, 0)
+        # )
+        
+        # self.configure(
+        #     'SelectedTabBtn.TLabel',
+        #     background='gray70',
+        #     padding=(5, 0, 5, 0)
+        # )
+        # self.map(
+        #     'SelectedTabBtn.TLabel',
+        #     background=(
+        #         ('pressed', 'gray60'), 
+        #         ('active', 'gray65')
+        #     )
+        # )
         
         
-        ##########
+        # ##########
