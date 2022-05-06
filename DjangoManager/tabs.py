107a8dcd76e2ef636a0a23a616448ce2cd4fca74
Corban-Lee <843224@student.derby-college.ac.uk>
2022-05-06 15:10:54 +0100

    added left border to moving tabs

--- DjangoManager/tabs.py
+++ DjangoManager/tabs.py
@@ -24,7 +24,7 @@ class Tab(ttk.Frame):
         
         # text displayed on the tab
         self.text = ttk.Label(self, text=text_length_check(text, 20))
-        self.text.grid(column=0, row=1, sticky='nsew')
+        self.text.grid(column=1, row=1, sticky='nsew')
         self.text.bind('<Button-1>', self.drag)
 
         # get light and dark 'x' images for close button
@@ -36,14 +36,12 @@ class Tab(ttk.Frame):
             self.close_imgs[color] = img
         
         self.close_btn = ttk.Button(self, command=self._destroy, cursor='hand2')
-        self.close_btn.grid(column=1, row=1, sticky='nes')
+        self.close_btn.grid(column=2, row=1, sticky='nes')
 
-        vert_sep = ttk.Frame(self, style='TabSeparator.TFrame', width=1)
-        vert_sep.grid(column=2, row=1, sticky='ns')
-        
-        # looks cleaner without this.
-        # horz_sep = ttk.Frame(self, style='TabSeparator.TFrame', height=1)
-        # horz_sep.grid(column=0, columnspan=3, row=0, sticky='we')
+        right_sep = ttk.Frame(self, style='TabSeparator.TFrame', width=1)
+        right_sep.grid(column=3, row=1, sticky='ns')
+
+        self.left_sep = ttk.Frame(self, style='TabSeparator.TFrame', width=1)
 
         # reserves a space when moving the tab
         self.reserved_space = ttk.Frame(self.master)
@@ -99,17 +97,22 @@ class Tab(ttk.Frame):
         self.reserved_space.config(width=self.winfo_width())
         self.reserved_space.pack(side='left', fill='y', before=self)
         
-        # if the tab isn't raised it can appear below other tabs and reserved space
+        # add left side border while moving
+        self.left_sep.grid(column=0, row=1, sticky='ns')
+        
+        # if the tab isn't raised it can appear below other
+        # tabs and reserved space
         self.tkraise()
         
-        original_x = self.winfo_x()
+        original_x = self.winfo_x() - 1  # account for left border
         current_x = original_x - event.x_root
         trough_height = self.master.winfo_height()
         
         def release(event:tkinter.Event) -> None:
             """Left mouse button is released"""
-
+            
             # create a list of everything on the tab trough excluding self
+            self.left_sep.grid_forget()
             items = self.master.tabs.copy()
             items.append(self.reserved_space)
             items.remove(self)
